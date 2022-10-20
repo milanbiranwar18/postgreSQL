04aff34d23319df02731c601fbd31fcd8622b4e6
milanbiranwar18 <109934967+milanbiranwar18@users.noreply.github.com>
2022-10-20 22:09:38 +0530

    Update order_by181022.sql

--- order_by181022.sql
+++ order_by181022.sql
@@ -1,18 +1,3 @@
-INSERT INTO accounts (username, password, email, created_on, last_login) VALUES('miln', 'Sample@123','abc@gmail.com', '12/2/2021', '2/8/2021');
-
-SELECT username, password FROM accounts ORDER BY username ASC, password DESC;
- 
- INSERT INTO accounts (username, password, email, created_on, last_login) VALUES('mohan', 'ample@123','bbc@gmail.com', '12/2/2021', '2/8/2021');
-
- INSERT INTO accounts (username, password, email, created_on, last_login) VALUES('raj', 'rample@123','cbbc@gmail.com', '12/2/2021', '2/8/2021');
-
- INSERT INTO accounts (username, password, email, created_on, last_login) VALUES('ravi', 'aample@123','rbbc@gmail.com', '12/2/2021', '2/8/2021');
-
- SELECT * FROM accounts;
-
- SELECT username, password FROM accounts ORDER BY username ASC, password DESC;
- 
-
  CREATE TABLE data ( id serial NOT NULL PRIMARY KEY, firstname VARCHAR, lastname VARCHAR );
 
 
@@ -28,3 +13,10 @@ SELECT username, password FROM accounts ORDER BY username ASC, password DESC;
 
  SELECT firstname, lastname FROM data ORDER BY firstname ASC, lastname DESC;
  
+ SELECT firstname, lastname FROM data ORDER BY firstname;
+ 
+ SELECT firstname, lastname FROM data ORDER BY firstname ASC;
+ 
+ SELECT firstname, lastname FROM data ORDER BY firstname;
+ 
+ SELECT firstname, lastname FROM data ORDER BY lastname DESC;
